511d19e8005a34ee94bba6bf7fb68aeb407a139b
Chihun Moon <cmoon@c4r9s6.42seoul.kr>
2022-12-22 03:47:30 +0900

    22/12/22 cmoon > Match 대기창 & 메인페이지  초안제작

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,51 +1,36 @@
-import MainLayout from '@/layouts/MainLayout';
-import Head from 'next/head';
-import { ReactElement, useEffect } from 'react';
+import MainLayout from "@/layouts/MainLayout";
+import Head from "next/head";
 
-import WinLoseSum from '@/components/match-history/WinLoseSum';
-import MatchHistory from '@/components/match-history/MatchHistory';
-import { MatchHistoryProps } from '@/interfaces/MatchProps';
+import { ReactElement } from "react";
+
+import WinLoseSum from "@/components/match-history/WinLoseSum";
+import MatchHistory from "@/components/match-history/MatchHistory";
+import { MatchHistoryProps } from "@/interfaces/MatchProps";
+import MainPage from "@/components/MainPage";
+
+//TODO: main이 GNB SNB 공간 주고 남은 부분을 꽉 채우게 하고싶은데 방법 모름.
 export default function LandingPage() {
-  // const { bears, increasePopulation, removeAllBears } = useBearStore();
-
-  // useEffect(() => {
-  //     console.log(bears);
-  // }, [bears]);
-
-  const dummyMatchHistory: MatchHistoryProps = {
-    myName: 'cmoon',
-    myScore: 10,
-    oppName: 'sunbchoi',
-    oppScore: 1,
-  };
-
-  return (
-    <>
-      <Head>
-        <title>LastPong</title>
-        <meta name="description" content="ft_transcendence project in 42 Seoul" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        {/* <button onClick={increasePopulation}>bear 늘리기</button>
-                <button onClick={removeAllBears}>bear 없애기</button>
-                <br />
-                <div>bear number : {bears}</div> */}
-        <WinLoseSum />
-        <MatchHistory
-          myName={dummyMatchHistory.myName}
-          myScore={dummyMatchHistory.myScore}
-          oppName={dummyMatchHistory.oppName}
-          oppScore={dummyMatchHistory.oppScore}
-        />
-      </main>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>LastPong</title>
+                <meta
+                    name="description"
+                    content="ft_transcendence project in 42 Seoul"
+                />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MainPage />
+        </>
+    );
 }
 
 LandingPage.getLayout = function (page: ReactElement) {
-  return <MainLayout>{page}</MainLayout>;
+    return <MainLayout>{page}</MainLayout>;
 };
 
 // LandingPage.getLayout = MainLayout;
