4ef67e1500b81ee6b962c0c0eccf1d1a74047368
Yongjun Park <yopark@student.42seoul.kr>
2022-12-22 04:58:27 +0900

    Feat:  CustomButton 컴포넌트 이용하여 LandingPage 재작성

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,36 +1,34 @@
-import MainLayout from "@/layouts/MainLayout";
-import Head from "next/head";
+import MainLayout from '@/layouts/MainLayout';
+import Head from 'next/head';
+import { ReactElement, useState } from 'react';
+import { Image } from '@chakra-ui/react';
+import Modal from '@/components/modal/Modal';
+import { CustomButton } from '@/components/CustomButton';
 
-import { ReactElement } from "react";
-
-import WinLoseSum from "@/components/match-history/WinLoseSum";
-import MatchHistory from "@/components/match-history/MatchHistory";
-import { MatchHistoryProps } from "@/interfaces/MatchProps";
-import MainPage from "@/components/MainPage";
-
-//TODO: main이 GNB SNB 공간 주고 남은 부분을 꽉 채우게 하고싶은데 방법 모름.
 export default function LandingPage() {
-    return (
-        <>
-            <Head>
-                <title>LastPong</title>
-                <meta
-                    name="description"
-                    content="ft_transcendence project in 42 Seoul"
-                />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <MainPage />
-        </>
-    );
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const toggleModalOpen = () => {
+    setIsModalOpen((cur) => !cur);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>LastPong</title>
+        <meta name="description" content="ft_transcendence project in 42 Seoul" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Image src="/HowToPlay.png" height="90%" alt="How To Play LastPong" />
+      <CustomButton size="lg" onClick={toggleModalOpen}>
+        MATCH
+      </CustomButton>
+      {isModalOpen ? <Modal closed={toggleModalOpen} /> : null}
+    </>
+  );
 }
 
 LandingPage.getLayout = function (page: ReactElement) {
-    return <MainLayout>{page}</MainLayout>;
+  return <MainLayout>{page}</MainLayout>;
 };
-
-// LandingPage.getLayout = MainLayout;
