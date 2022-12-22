0bf4f4bc24a245304ac0ce0e11b963b8252af7dc
Yongjun Park <yopark@student.42seoul.kr>
2022-12-22 17:14:03 +0900

    Refactor: BasicLayout 수정

--- frontend/src/layouts/BasicLayout.tsx
+++ frontend/src/layouts/BasicLayout.tsx
@@ -1,27 +1,14 @@
-import { ReactNode } from "react";
-import Header from "@/components/Header";
-import { Flex } from "@chakra-ui/react";
+import { ReactNode } from 'react';
+import { Center, VStack } from '@chakra-ui/react';
 
 type BasicLayoutProps = {
   children: ReactNode;
 };
 
 export default function BasicLayout({ children }: BasicLayoutProps) {
-  const myUsername = "yopark";
-  const styles = {
-    BasicStyle: {
-      width: "100%",
-      height: "100vh",
-      backgroundColor: "#4267b2",
-      alignItems: "center",
-      justifyContent: "center",
-    } as React.CSSProperties,
-  };
   return (
-    <>
-      <Flex style={styles.BasicStyle}>
-        <main>{children}</main>
-      </Flex>
-    </>
+    <Center bg="main" color="white">
+      <VStack>{children}</VStack>
+    </Center>
   );
 }
