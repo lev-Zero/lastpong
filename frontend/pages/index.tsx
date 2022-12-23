56caa2fd28a64f1f088e5ede9f1ead8f19f1b972
Yongjun Park <yopark@student.42seoul.kr>
2022-12-23 09:48:09 +0900

    Feat: Countdown Modal 구현

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,40 +1,66 @@
-import MainLayout from "@/layouts/MainLayout";
-import Head from "next/head";
-import { ReactElement, useState } from "react";
-import { Flex, Image } from "@chakra-ui/react";
-import Modal from "@/components/modal/Modal";
-import { CustomButton } from "@/components/CustomButton";
+import MainLayout from '@/layouts/MainLayout';
+import Head from 'next/head';
+import { ReactElement, useEffect, useState } from 'react';
+import {
+  Center,
+  Flex,
+  Image,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+  VStack,
+} from '@chakra-ui/react';
+import { CustomButton } from '@/components/CustomButton';
 
 export default function LandingPage() {
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [timeSpent, setTimeSpent] = useState<number>(1);
 
-  const toggleModalOpen = () => {
-    setIsModalOpen((cur) => !cur);
-  };
+  useEffect(() => {
+    const interval = setInterval(() => setTimeSpent((cur) => cur + 1), 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    if (isOpen) {
+      setTimeSpent(1);
+    }
+  }, [isOpen]);
 
   return (
     <>
       <Head>
         <title>LastPong</title>
-        <meta
-          name="description"
-          content="ft_transcendence project in 42 Seoul"
-        />
+        <meta name="description" content="ft_transcendence project in 42 Seoul" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Flex
-        height={"100%"}
-        flexDir={"column"}
-        alignItems="center"
-        justifyContent={"center"}
-      >
+      <Flex height={'100%'} flexDir={'column'} alignItems="center" justifyContent={'center'}>
         <Image src="/HowToPlay.png" height="90%" alt="How To Play LastPong" />
-        <CustomButton size="lg" onClick={toggleModalOpen}>
+        <CustomButton size="lg" onClick={onOpen}>
           MATCH
         </CustomButton>
-        {isModalOpen ? <Modal closed={toggleModalOpen} /> : null}
       </Flex>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent bg="main" color="white">
+          <Center>
+            <VStack>
+              <ModalHeader>LOOKING FOR AN OPPONENT...</ModalHeader>
+              <ModalBody fontSize="6xl">{timeSpent}</ModalBody>
+              <ModalFooter>
+                <CustomButton size="md" onClick={onClose}>
+                  CANCEL
+                </CustomButton>
+              </ModalFooter>
+            </VStack>
+          </Center>
+        </ModalContent>
+      </Modal>
     </>
   );
 }
