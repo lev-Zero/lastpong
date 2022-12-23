09b3d2fdd2f66aa62f40a42c195c5c9ce7b62fb3
Sunbin Choi <sunbchoi@c5r4s2.42seoul.kr>
2022-12-23 21:53:38 +0900

    AUTH CHECK

---
+++ frontend/pages/auth/login.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Head from "next/head";
+import { ReactElement } from "react";
+import { CustomButton } from "@/components/CustomButton";
+import { Center, Text } from "@chakra-ui/react";
+import Link from "next/link";
+
+import useLoginStore from "@/store/useLoginStore";
+import BasicLayout from "@/layouts/BasicLayout";
+
+const styles = {
+  MainText: {
+    fontSize: "140px",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDir: "column",
+  } as React.CSSProperties,
+};
+
+function onLoginUser() {
+  console.log("Clicked!");
+
+  const getAuth = async () => {
+    const res = await fetch("http://localhost:3000/auth", {
+      headers: { "Access-Control-Allow-Origin": "http://localhost:3000" },
+    });
+  };
+  getAuth();
+  return;
+}
+
+export default function AuthLoginPage() {
+  return (
+    <>
+      <Head>
+        <title>LastPong</title>
+        <meta
+          name="description"
+          content="ft_transcendence project in 42 Seoul"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Center flexDir={"column"}>
+          <Text style={styles.MainText}> LAST PONG </Text>
+          <CustomButton size="md" onClick={onLoginUser}>
+            START
+          </CustomButton>
+        </Center>
+      </main>
+    </>
+  );
+}
+
+AuthLoginPage.getLayout = function (page: ReactElement) {
+  return <BasicLayout>{page}</BasicLayout>;
+};
