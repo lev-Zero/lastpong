afdea12706838560f88ee102e5202f88cfa5da21
Yongjun Park <yopark@student.42seoul.kr>
2022-12-23 22:07:49 +0900

    Feat: /chat 뷰 제작

--- frontend/pages/chat/index.tsx
+++ frontend/pages/chat/index.tsx
@@ -1,8 +1,39 @@
+import { ChatRoomItem, ChatRoomItemProps } from '@/components/ChatRoomItem';
+import UserItem from '@/components/UserItem';
+import { UserProps, UserStatus } from '@/interfaces/UserProps';
 import MainLayout from '@/layouts/MainLayout';
+import { Box, Flex, HStack, SimpleGrid, VStack } from '@chakra-ui/react';
 import Head from 'next/head';
 import { ReactElement } from 'react';
 
 export default function ChatPage() {
+  const friend: UserProps = {
+    name: 'yopark',
+    imgUrl: '',
+    status: UserStatus.inGame,
+    rating: 1028,
+    winCnt: 3,
+    loseCnt: 2,
+    useOtp: false,
+  };
+
+  const owner: UserProps = {
+    name: 'yopark',
+    imgUrl: '',
+    status: UserStatus.inGame,
+    rating: 1028,
+    winCnt: 3,
+    loseCnt: 2,
+    useOtp: false,
+  };
+
+  const dummyChatRoom: ChatRoomItemProps = {
+    title: 'Lonely Night Chat',
+    owner: owner,
+    isPrivate: true,
+    password: '1234',
+  };
+
   return (
     <>
       <Head>
@@ -10,7 +41,84 @@ export default function ChatPage() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>채팅 페이지</main>
+      <Flex w="100%" h="100%">
+        <Box w="70%" h="90%" overflowY="scroll">
+          <SimpleGrid m={20} columns={2} spacing={5}>
+            <ChatRoomItem
+              title={dummyChatRoom.title}
+              owner={dummyChatRoom.owner}
+              isPrivate={false}
+              password={dummyChatRoom.password}
+            />
+            <ChatRoomItem
+              title={dummyChatRoom.title}
+              owner={dummyChatRoom.owner}
+              isPrivate={dummyChatRoom.isPrivate}
+              password={dummyChatRoom.password}
+            />
+            <ChatRoomItem
+              title={dummyChatRoom.title}
+              owner={dummyChatRoom.owner}
+              isPrivate={dummyChatRoom.isPrivate}
+              password={dummyChatRoom.password}
+            />
+            <ChatRoomItem
+              title={dummyChatRoom.title}
+              owner={dummyChatRoom.owner}
+              isPrivate={false}
+              password={dummyChatRoom.password}
+            />
+            <ChatRoomItem
+              title={dummyChatRoom.title}
+              owner={dummyChatRoom.owner}
+              isPrivate={dummyChatRoom.isPrivate}
+              password={dummyChatRoom.password}
+            />
+            <ChatRoomItem
+              title={dummyChatRoom.title}
+              owner={dummyChatRoom.owner}
+              isPrivate={dummyChatRoom.isPrivate}
+              password={dummyChatRoom.password}
+            />
+            <ChatRoomItem
+              title={dummyChatRoom.title}
+              owner={dummyChatRoom.owner}
+              isPrivate={dummyChatRoom.isPrivate}
+              password={dummyChatRoom.password}
+            />
+            <ChatRoomItem
+              title={dummyChatRoom.title}
+              owner={dummyChatRoom.owner}
+              isPrivate={dummyChatRoom.isPrivate}
+              password={dummyChatRoom.password}
+            />
+            <ChatRoomItem
+              title={dummyChatRoom.title}
+              owner={dummyChatRoom.owner}
+              isPrivate={dummyChatRoom.isPrivate}
+              password={dummyChatRoom.password}
+            />
+            <ChatRoomItem
+              title={dummyChatRoom.title}
+              owner={dummyChatRoom.owner}
+              isPrivate={dummyChatRoom.isPrivate}
+              password={dummyChatRoom.password}
+            />
+          </SimpleGrid>
+        </Box>
+        <VStack w="25%" h="90%" m={10} p={7} backgroundColor="white">
+          <VStack w="100%" overflowY="scroll">
+            <UserItem user={friend} />
+            <UserItem user={friend} />
+            <UserItem user={friend} />
+            <UserItem user={friend} />
+            <UserItem user={friend} />
+            <UserItem user={friend} />
+            <UserItem user={friend} />
+            <UserItem user={friend} />
+          </VStack>
+        </VStack>
+      </Flex>
     </>
   );
 }
