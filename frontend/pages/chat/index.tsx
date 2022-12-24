74a1eb6654236feb5b1c30e4ff37cb03b6d03c13
Yongjun Park <yopark@student.42seoul.kr>
2022-12-24 15:58:00 +0900

    Feat: /chat CREATE 버튼 추가

--- frontend/pages/chat/index.tsx
+++ frontend/pages/chat/index.tsx
@@ -1,4 +1,5 @@
 import { ChatRoomItem, ChatRoomItemProps } from '@/components/ChatRoomItem';
+import { CustomButton } from '@/components/CustomButton';
 import UserItem from '@/components/UserItem';
 import { UserProps, UserStatus } from '@/interfaces/UserProps';
 import MainLayout from '@/layouts/MainLayout';
@@ -42,70 +43,77 @@ export default function ChatPage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Flex w="100%" h="100%">
-        <Box w="70%" h="90%" overflowY="scroll">
-          <SimpleGrid m={20} columns={2} spacing={5}>
-            <ChatRoomItem
-              title={dummyChatRoom.title}
-              owner={dummyChatRoom.owner}
-              isPrivate={false}
-              password={dummyChatRoom.password}
-            />
-            <ChatRoomItem
-              title={dummyChatRoom.title}
-              owner={dummyChatRoom.owner}
-              isPrivate={dummyChatRoom.isPrivate}
-              password={dummyChatRoom.password}
-            />
-            <ChatRoomItem
-              title={dummyChatRoom.title}
-              owner={dummyChatRoom.owner}
-              isPrivate={dummyChatRoom.isPrivate}
-              password={dummyChatRoom.password}
-            />
-            <ChatRoomItem
-              title={dummyChatRoom.title}
-              owner={dummyChatRoom.owner}
-              isPrivate={false}
-              password={dummyChatRoom.password}
-            />
-            <ChatRoomItem
-              title={dummyChatRoom.title}
-              owner={dummyChatRoom.owner}
-              isPrivate={dummyChatRoom.isPrivate}
-              password={dummyChatRoom.password}
-            />
-            <ChatRoomItem
-              title={dummyChatRoom.title}
-              owner={dummyChatRoom.owner}
-              isPrivate={dummyChatRoom.isPrivate}
-              password={dummyChatRoom.password}
-            />
-            <ChatRoomItem
-              title={dummyChatRoom.title}
-              owner={dummyChatRoom.owner}
-              isPrivate={dummyChatRoom.isPrivate}
-              password={dummyChatRoom.password}
-            />
-            <ChatRoomItem
-              title={dummyChatRoom.title}
-              owner={dummyChatRoom.owner}
-              isPrivate={dummyChatRoom.isPrivate}
-              password={dummyChatRoom.password}
-            />
-            <ChatRoomItem
-              title={dummyChatRoom.title}
-              owner={dummyChatRoom.owner}
-              isPrivate={dummyChatRoom.isPrivate}
-              password={dummyChatRoom.password}
-            />
-            <ChatRoomItem
-              title={dummyChatRoom.title}
-              owner={dummyChatRoom.owner}
-              isPrivate={dummyChatRoom.isPrivate}
-              password={dummyChatRoom.password}
-            />
-          </SimpleGrid>
-        </Box>
+        <VStack w="70%" h="90%" my={10}>
+          <Box overflowY="scroll" mb={10}>
+            <SimpleGrid columns={2} spacing={5}>
+              <ChatRoomItem
+                title={dummyChatRoom.title}
+                owner={dummyChatRoom.owner}
+                isPrivate={false}
+                password={dummyChatRoom.password}
+              />
+              <ChatRoomItem
+                title={dummyChatRoom.title}
+                owner={dummyChatRoom.owner}
+                isPrivate={dummyChatRoom.isPrivate}
+                password={dummyChatRoom.password}
+              />
+              <ChatRoomItem
+                title={dummyChatRoom.title}
+                owner={dummyChatRoom.owner}
+                isPrivate={dummyChatRoom.isPrivate}
+                password={dummyChatRoom.password}
+              />
+              <ChatRoomItem
+                title={dummyChatRoom.title}
+                owner={dummyChatRoom.owner}
+                isPrivate={false}
+                password={dummyChatRoom.password}
+              />
+              <ChatRoomItem
+                title={dummyChatRoom.title}
+                owner={dummyChatRoom.owner}
+                isPrivate={dummyChatRoom.isPrivate}
+                password={dummyChatRoom.password}
+              />
+              <ChatRoomItem
+                title={dummyChatRoom.title}
+                owner={dummyChatRoom.owner}
+                isPrivate={dummyChatRoom.isPrivate}
+                password={dummyChatRoom.password}
+              />
+              <ChatRoomItem
+                title={dummyChatRoom.title}
+                owner={dummyChatRoom.owner}
+                isPrivate={dummyChatRoom.isPrivate}
+                password={dummyChatRoom.password}
+              />
+              <ChatRoomItem
+                title={dummyChatRoom.title}
+                owner={dummyChatRoom.owner}
+                isPrivate={dummyChatRoom.isPrivate}
+                password={dummyChatRoom.password}
+              />
+              <ChatRoomItem
+                title={dummyChatRoom.title}
+                owner={dummyChatRoom.owner}
+                isPrivate={dummyChatRoom.isPrivate}
+                password={dummyChatRoom.password}
+              />
+              <ChatRoomItem
+                title={dummyChatRoom.title}
+                owner={dummyChatRoom.owner}
+                isPrivate={dummyChatRoom.isPrivate}
+                password={dummyChatRoom.password}
+              />
+            </SimpleGrid>
+          </Box>
+          <Box>
+            <CustomButton size="lg" onClick={() => {}}>
+              CREATE
+            </CustomButton>
+          </Box>
+        </VStack>
         <VStack w="25%" h="90%" m={10} p={7} backgroundColor="white">
           <VStack w="100%" overflowY="scroll">
             <UserItem user={friend} />
