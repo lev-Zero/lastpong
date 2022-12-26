cc92f5758aef75486c358d41a5b04dbadee1512e
Yongjun Park <yopark@student.42seoul.kr>
2022-12-27 05:13:30 +0900

    minor changes

--- frontend/src/components/CustomButton.tsx
+++ frontend/src/components/CustomButton.tsx
@@ -1,43 +1,37 @@
-import { Button } from "@chakra-ui/react";
-import { ReactNode } from "react";
+import { Button } from '@chakra-ui/react';
+import { ReactNode } from 'react';
 
 export interface CustomButtonProps {
-    children: ReactNode;
-    size: string;
-    isDisabled?: boolean;
-    btnStyle?: object;
-    onClick: () => void;
+  children: ReactNode;
+  size: string;
+  isDisabled?: boolean;
+  btnStyle?: object;
+  onClick: () => void;
 }
 
 export function CustomButton({
-    children,
-    size,
-    isDisabled = false,
-    onClick,
-    btnStyle,
+  children,
+  size,
+  isDisabled = false,
+  onClick,
+  btnStyle,
 }: CustomButtonProps) {
-    const [bg, color]: [string, string] = !isDisabled
-        ? ["main", "white"]
-        : ["gray.200", "#AAAAAA"];
+  const [bg, color]: [string, string] = !isDisabled ? ['main', 'white'] : ['gray.200', '#AAAAAA'];
 
-    if (size === "xl") {
-        const style = {};
-    }
-
-    return (
-        <Button
-            w={120}
-            bg={bg}
-            color={color}
-            border="2px"
-            borderColor="white"
-            borderRadius="4242"
-            size={size}
-            disabled={isDisabled}
-            style={btnStyle}
-            onClick={onClick}
-        >
-            {children}
-        </Button>
-    );
+  return (
+    <Button
+      w={120}
+      bg={bg}
+      color={color}
+      border="2px"
+      borderColor="white"
+      borderRadius="4242"
+      size={size}
+      disabled={isDisabled}
+      style={btnStyle}
+      onClick={onClick}
+    >
+      {children}
+    </Button>
+  );
 }
