bc53636abf9c1e9f57b4799bf727e6a4808e77ae
choi-sunbin <csn1434@naver.com>
2022-12-26 21:57:30 +0900

    URL TO FILE

--- frontend/pages/auth/basicAvatar/index.tsx
+++ frontend/pages/auth/basicAvatar/index.tsx
@@ -49,21 +49,38 @@ const styles = {
 export default function BasicAvatarPage() {
   const selectFile = useRef<HTMLInputElement>();
   const selectAvatar = useRef<HTMLSpanElement>();
-  const [ImageFile, setImageFile] = useState<any>(null);
+  const [imageFile, setImageFile] = useState<any>(null);
   const { avatarImg, setAvatarImg } = useLoginStore();
 
-  const onChangeInput = async (event: React.ChangeEvent<HTMLInputElement>) => {
+  function FileToImg(file: any) {
     const reader = new FileReader();
+    const AvatarFile = file;
+    reader.readAsDataURL(AvatarFile);
+    reader.onloadend = () => {
+      setAvatarImg(reader.result);
+    };
+  }
+
+  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.currentTarget.files !== null) {
       if (event.currentTarget.files.length === 0) return;
-      const file = event.currentTarget.files[0];
-      console.log(file);
-      reader.readAsDataURL(file);
-      reader.onloadend = () => {
-        setImageFile(reader.result);
-        setAvatarImg(reader.result);
-      };
+      // console.log(imageFile);
+      // console.log(event.currentTarget.files[0]);
+      // setImageFile(event.currentTarget.files[0]);
+      // console.log(imageFile);
+      setImageFile(event.currentTarget.files[0]);
+      FileToImg(event.currentTarget.files[0]);
     }
+  };
+
+  const urlToObject = async (imageUrl: string) => {
+    const response = await fetch(imageUrl);
+    const blob = await response.blob();
+    const file = new File([blob], "image.jpg", { type: blob.type });
+    const dataTransfer = new DataTransfer();
+    dataTransfer.items.add(file);
+    if (selectFile.current) selectFile.current.files = dataTransfer.files;
+    FileToImg(file);
   };
 
   const onClickNext = (event: React.MouseEvent<HTMLElement>) => {
@@ -104,47 +121,29 @@ export default function BasicAvatarPage() {
         profilePhoto = json.profilePhoto;
         userName = json.username;
         console.log(json);
-      });
-
-    if (profilePhoto === undefined) {
-      console.log("AVATAR EMPTY");
-      headers = headers42;
-      var decodeURL = decodeURIComponent(cookies["profileUrl"]);
-
-      fetch(decodeURL, { headers })
-        .then((response) => response.json())
-        .then((json) => {
-          setAvatarImg(json.image.link);
-          profilePhoto = json.image.link;
-          console.log(profilePhoto);
-        });
-    }
-
-    var myFile;
-    fetch(profilePhoto)
-      .then((res) => res.blob())
-      .then((myBlob) => {
-        // debugger;
-        myFile = new File(["File"], "image.jpeg", { type: myBlob.type });
-        return myFile;
       })
-      .then((ImageFile) => {
-        var form = new FormData();
-        form.append("file", ImageFile, "image.jpg");
-        console.log(form.getAll("file"));
-        const body = { file: form };
-        headers = headersOTP;
-
-        fetch(SERVER_URL + "/user/avatar/me", {
-          method: "PUT",
-          body: JSON.stringify(body),
-          headers: headers,
-        })
-          .then((res) => res.json())
-          .then((json) => console.log(json));
+      .then(() => {
+        if (profilePhoto === undefined || profilePhoto === "empty") {
+          headers = headers42;
+          var decodeURL = decodeURIComponent(cookies["profileUrl"]);
+          fetch(decodeURL, { headers })
+            .then((response) => response.json())
+            .then((json) => {
+              profilePhoto = json.image.link;
+              urlToObject(profilePhoto);
+            });
+        }
       });
 
-    console.log(myFile);
+    //   fetch(SERVER_URL + "/user/avatar/me", {
+    //     method: "PUT",
+    //     body: form,
+    //     // headers: headers,
+    //   })
+    //     .then((res) => res.json())
+    //     .then((json) => console.log(json));
+    // });
+    // console.log(myFile);
   };
 
   return (
@@ -153,14 +152,6 @@ export default function BasicAvatarPage() {
         <main>
           <Flex style={styles.CenterFlex}>
             <Text style={styles.MenualText}>PLEASE UPLOAD YOUR AVATAR</Text>
-            <Input
-              type={"file"}
-              style={{ display: "none" }}
-              ref={selectFile}
-              accept={"image/*"}
-              onChange={onChangeInput}
-              id={"realPath"}
-            />
             <Flex
               width={"300px"}
               height={"300px"}
@@ -186,12 +177,28 @@ export default function BasicAvatarPage() {
             >
               UPLOAD
             </Button>
-            <Button style={styles.ThemaButton} onClick={onClickNext}>
-              NEXT
-            </Button>
+
             <Button style={styles.ThemaButton} onClick={onClickCheck}>
               Avatar ME TEST
             </Button>
+            <form
+              name="accountFrm"
+              method="put"
+              encType="multipart/form-data"
+              style={{ color: "black" }}
+            >
+              <input
+                type="file"
+                ref={selectFile}
+                style={{ display: "none" }}
+                accept={"image/*"}
+                name="profile_img"
+                onChange={onChangeInput}
+              />
+              <Button style={styles.ThemaButton} onClick={onClickNext}>
+                NEXT
+              </Button>
+            </form>
           </Flex>
         </main>
       </BasicLayout>
