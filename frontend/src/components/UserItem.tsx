f021d8f339f4b7bf0cb4bb479b68b39422f83f34
Yongjun Park <yopark@student.42seoul.kr>
2022-12-28 01:50:50 +0900

    Feat: DM 창 뷰 개발 (미완)

--- frontend/src/components/UserItem.tsx
+++ frontend/src/components/UserItem.tsx
@@ -1,7 +1,23 @@
 import { UserProps } from '@/interfaces/UserProps';
-import { Box, Circle, Flex, MenuItem, MenuList, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Circle,
+  Flex,
+  HStack,
+  MenuItem,
+  MenuList,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
+  Portal,
+  Text,
+} from '@chakra-ui/react';
 import { ContextMenu } from 'chakra-ui-contextmenu';
-import Link from 'next/link';
 import CustomAvatar from './CustomAvatar';
 import { OptionMenu } from './OptionMenu';
 
@@ -10,38 +26,68 @@ interface UserItemProps {
   msgNum?: number;
 }
 
-export default function UserItem({ user, msgNum }: UserItemProps) {
+function UserItem({ user, msgNum }: UserItemProps) {
+  return (
+    <>
+      {msgNum !== undefined && msgNum !== 0 ? (
+        <Circle
+          position="absolute"
+          size="40px"
+          transform="translate(-30%, -30%)"
+          bg="#FF3D00"
+          color="white"
+        >
+          {msgNum}
+        </Circle>
+      ) : null}
+      <Flex
+        w="100%"
+        justifyContent="space-around"
+        alignItems="center"
+        p={4}
+        bg="gray.100"
+        border="2px"
+        borderRadius={20}
+      >
+        <CustomAvatar url={user.imgUrl} size="md" status={user.status} />
+        <Text>{user.name.toUpperCase()}</Text>
+      </Flex>
+    </>
+  );
+}
+
+function PopoverHoc({ user, msgNum }: UserItemProps) {
+  return (
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Box>
+          <UserItem user={user} msgNum={msgNum} />
+        </Box>
+      </PopoverTrigger>
+      <Portal>
+        <PopoverContent borderRadius="20">
+          <PopoverCloseButton />
+          <PopoverHeader borderTopRadius="20" p="3" bg="main" color="white">
+            <HStack spacing="5">
+              <CustomAvatar url={user.imgUrl} size="md" status={user.status} />
+              <Text fontSize="xl">{user.name.toUpperCase()}</Text>
+            </HStack>
+          </PopoverHeader>
+          <PopoverBody>Are you sure you want to have that milkshake?</PopoverBody>
+        </PopoverContent>
+      </Portal>
+    </Popover>
+  );
+}
+
+export default function ContextMenuHoc({ user, msgNum }: UserItemProps) {
   return (
     <ContextMenu<HTMLDivElement>
       renderMenu={() => <OptionMenu user={user} isFriend={true} isBlocked={false} />}
     >
       {(ref) => (
         <Box ref={ref} w="100%" position="relative" px={3} py={1}>
-          <Link href={`/user/${user.name}`}>
-            {msgNum !== undefined && msgNum !== 0 ? (
-              <Circle
-                position="absolute"
-                size="40px"
-                transform="translate(-30%, -30%)"
-                bg="#FF3D00"
-                color="white"
-              >
-                {msgNum}
-              </Circle>
-            ) : null}
-            <Flex
-              w="100%"
-              justifyContent="space-around"
-              alignItems="center"
-              p={4}
-              bg="gray.100"
-              border="2px"
-              borderRadius={20}
-            >
-              <CustomAvatar url={user.imgUrl} size="md" status={user.status} />
-              <Text>{user.name.toUpperCase()}</Text>
-            </Flex>
-          </Link>
+          <PopoverHoc user={user} msgNum={msgNum} />
         </Box>
       )}
     </ContextMenu>
