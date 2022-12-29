a40ab1426536d0e1e534161c8186053906d2188e
Yongjun Park <yopark@student.42seoul.kr>
2022-12-29 14:32:30 +0900

    Feat: profile 페이지 유저 정보 실제로 변경 (대전 기록은 제외)

--- frontend/pages/user/[username].tsx
+++ frontend/pages/user/[username].tsx
@@ -6,33 +6,53 @@ import { MatchHistoryProps } from '@/interfaces/MatchProps';
 import { UserProps, UserStatus } from '@/interfaces/UserProps';
 import MainLayout from '@/layouts/MainLayout';
 import { userStore } from '@/stores/userStore';
+import { customFetch } from '@/utils/customFetch';
 import { Box, Center, Divider, Flex, HStack, Text, VStack } from '@chakra-ui/react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { ReactElement } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 
 export default function UserProfilePage() {
   const router = useRouter();
   let rawUsername: string | string[] | undefined = router.query.username;
   let username: string = '';
   if (rawUsername === undefined) {
-    username = '';
+    username = ''; // FIXME: 에러 핸들링
   } else if (Array.isArray(rawUsername)) {
-    username = rawUsername.join('');
+    username = rawUsername.join(''); // FIXME: 에러 핸들링
   } else {
     username = rawUsername;
   }
 
-  const { addFriend, deleteFriend } = userStore();
-  const user: UserProps = {
-    name: username,
-    imgUrl: '',
-    status: UserStatus.inGame,
-    rating: 1028,
-    winCnt: 3,
-    loseCnt: 2,
-    useOtp: false,
-  };
+  const { addFriend } = userStore();
+
+  const [user, setUser] = useState<UserProps | null>(null);
+
+  useEffect(() => {
+    async function setUserInfo() {
+      try {
+        const json = await customFetch('GET', `/user/name/${username}`);
+        const fetchedUser = {
+          name: json.username,
+          imgUrl: '', // TODO : img는 따로 가져와야 한다.
+          status: json.status,
+          rating: json.rating,
+          useOtp: false,
+        };
+        console.log('fetchedUser', fetchedUser);
+        setUser(fetchedUser);
+      } catch (e) {
+        if (e instanceof Error) {
+          console.log(e.message);
+          return;
+        }
+      }
+    }
+    setUserInfo();
+  }, []);
+
+  const winCnt = 42;
+  const loseCnt = 42;
 
   const dummyMatchHistory: MatchHistoryProps = {
     myName: username,
@@ -42,66 +62,62 @@ export default function UserProfilePage() {
   };
 
   return (
-    <VStack>
-      <Head>
-        <title>{`${username} | LastPong`}</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Flex
-        bg="white"
-        flexDirection="column"
-        borderRadius={42}
-        w="40%"
-        minW="500px"
-        margin="auto"
-        alignItems="center"
-        justifyContent="center"
-        my={30}
-        p={20}
-      >
-        <HStack>
-          <Box mr={10}>
-            <CustomAvatar url={user.imgUrl} size="xl" />
-          </Box>
-          <VStack>
-            <Text fontSize="3xl" color="main">
-              {user.name.toUpperCase()}
-            </Text>
-            <Text fontSize="lg">{`RATING ${user.rating}`}</Text>
-          </VStack>
-        </HStack>
-        <Divider border="1px" borderColor="main" my={10} />
-        <WinLoseSum winCnt={user.winCnt} loseCnt={user.loseCnt} fontSize="3xl" />
-        <VStack w="100%" my={10} maxH="15vh" overflowY="scroll">
-          {[...Array(10)].map((_, idx) => (
-            <MatchHistory
-              key={idx}
-              myName={dummyMatchHistory.myName}
-              myScore={dummyMatchHistory.myScore}
-              oppName={dummyMatchHistory.oppName}
-              oppScore={dummyMatchHistory.oppScore}
-            />
-          ))}
+    <>
+      {user === null ? null : (
+        <VStack>
+          <Head>
+            <title>{`${username} | LastPong`}</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <Flex
+            bg="white"
+            flexDirection="column"
+            borderRadius={42}
+            w="40%"
+            minW="500px"
+            margin="auto"
+            alignItems="center"
+            justifyContent="center"
+            my={30}
+            p={20}
+          >
+            <HStack>
+              <Box mr={10}>
+                <CustomAvatar url={user.imgUrl} size="xl" />
+              </Box>
+              <VStack>
+                <Text fontSize="3xl" color="main">
+                  {user.name.toUpperCase()}
+                </Text>
+                <Text fontSize="lg">{`RATING ${user.rating}`}</Text>
+              </VStack>
+            </HStack>
+            <Divider border="1px" borderColor="main" my={10} />
+            <WinLoseSum winCnt={winCnt} loseCnt={loseCnt} fontSize="3xl" />
+            <VStack w="100%" my={10} maxH="15vh" overflowY="scroll">
+              {[...Array(10)].map((_, idx) => (
+                <MatchHistory
+                  key={idx}
+                  myName={dummyMatchHistory.myName}
+                  myScore={dummyMatchHistory.myScore}
+                  oppName={dummyMatchHistory.oppName}
+                  oppScore={dummyMatchHistory.oppScore}
+                />
+              ))}
+            </VStack>
+          </Flex>
+          <CustomButton
+            size="xl"
+            onClick={() => {
+              addFriend(username);
+            }}
+          >
+            ADD FRIEND
+          </CustomButton>
         </VStack>
-      </Flex>
-      <CustomButton
-        size="xl"
-        onClick={() => {
-          addFriend(username);
-        }}
-      >
-        ADD FRIEND
-      </CustomButton>
-      <CustomButton
-        size="xl"
-        onClick={() => {
-          deleteFriend(username);
-        }}
-      >
-        DEL FRIEND
-      </CustomButton>
-    </VStack>
+      )}
+    </>
   );
 }
 
