17061759eb482bfa7bb00d81d9574ada909b39b1
Yongjun Park <yopark@student.42seoul.kr>
2023-01-02 20:09:34 +0900

    Refactor: InviteModal 밖으로 빼기

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
-import { ReactElement, ReactNode } from 'react';
+import { ReactElement, ReactNode, useEffect } from 'react';
 import theme from './theme';
 import '/styles/global.css';
 import {
@@ -22,11 +22,10 @@ import {
   HStack,
   Link,
 } from '@chakra-ui/react';
-import { SERVER_URL } from '@/utils/variables';
 import { CustomButton } from '@/components/CustomButton';
 import CustomAvatar from '@/components/CustomAvatar';
 import { UserProps, UserStatus } from '@/interfaces/UserProps';
-import { useRouter } from 'next/router';
+import { chatStore } from '@/stores/chatStore';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -36,11 +35,9 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
+function InviteModal() {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const router = useRouter();
 
-  const getLayout = Component.getLayout ?? ((page) => page);
   const invitingDummyUser: UserProps = {
     id: 1,
     name: 'yopark',
@@ -50,57 +47,66 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
   };
 
   return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalOverlay />
+      <ModalContent bg="win" color="white" borderRadius={30}>
+        <Center>
+          <VStack>
+            <ModalBody>
+              <VStack>
+                <Flex w="100%" justifyContent="space-around" alignItems="center" bg="Win">
+                  <Box mr={5}>
+                    <CustomAvatar
+                      url={invitingDummyUser.imgUrl}
+                      size="xl"
+                      status={invitingDummyUser.status}
+                    />
+                  </Box>
+                  <VStack>
+                    <Text fontSize="4xl">{invitingDummyUser.name.toUpperCase()}</Text>
+                    <HStack>
+                      <Text>RATING</Text>
+                      <Text color={'opponent'}>{invitingDummyUser.rating}</Text>
+                    </HStack>
+                  </VStack>
+                </Flex>
+                <Text fontSize="300%">INVITED YOU</Text>
+              </VStack>
+            </ModalBody>
+            <ModalFooter>
+              <VStack mb={'7'}>
+                {/* TODO:onclick 핸들러로 매치 잡는 기능 */}
+                <CustomButton size="lg" onClick={onClose}>
+                  MATCH
+                </CustomButton>
+                <CustomButton
+                  size="lg"
+                  onClick={onClose}
+                  btnStyle={{
+                    background: 'transparent',
+                  }}
+                >
+                  CANCEL
+                </CustomButton>
+              </VStack>
+            </ModalFooter>
+          </VStack>
+        </Center>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+  const { makeSocket } = chatStore();
+
+  useEffect(() => {}, []);
+
+  return (
     <ChakraProvider theme={theme}>
       {getLayout(<Component {...pageProps} />)}
-      <Modal isOpen={isOpen} onClose={onClose} isCentered>
-        <ModalOverlay />
-        <ModalContent bg="win" color="white" borderRadius={30}>
-          <Center>
-            <VStack>
-              <ModalHeader></ModalHeader>
-              <ModalBody>
-                <VStack>
-                  <Flex w="100%" justifyContent="space-around" alignItems="center" bg="Win">
-                    <Box mr={5}>
-                      <CustomAvatar
-                        url={invitingDummyUser.imgUrl}
-                        size="xl"
-                        status={invitingDummyUser.status}
-                      />
-                    </Box>
-                    <VStack>
-                      <Text fontSize="4xl">{invitingDummyUser.name.toUpperCase()}</Text>
-                      <HStack>
-                        <Text>RATING</Text>
-                        <Text color={'opponent'}>{invitingDummyUser.rating}</Text>
-                      </HStack>
-                    </VStack>
-                  </Flex>
-                  <Text fontSize="300%">INVITED YOU</Text>
-                </VStack>
-              </ModalBody>
-              <ModalFooter>
-                <VStack mb={'7'}>
-                  {/* TODO:onclick 핸들러로 매치 잡는 기능 */}
-                  <CustomButton size="lg" onClick={onClose}>
-                    MATCH
-                  </CustomButton>
-                  <CustomButton
-                    size="lg"
-                    onClick={onClose}
-                    btnStyle={{
-                      background: 'transparent',
-                    }}
-                  >
-                    CANCEL
-                  </CustomButton>
-                </VStack>
-              </ModalFooter>
-            </VStack>
-          </Center>
-        </ModalContent>
-      </Modal>
-      {/* <Button onClick={onOpen}>Invited</Button> */}
+      <InviteModal />
     </ChakraProvider>
   );
 }
