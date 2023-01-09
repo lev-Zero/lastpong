1fbac4f45e197122529c1e92433bf9291aec5719
Yongjun Park <yopark@student.42seoul.kr>
2023-01-09 17:18:50 +0900

    Feat: DM창 로직 수정 중 (보내는 것 빼고 완성)

--- frontend/src/components/UserItem.tsx
+++ frontend/src/components/UserItem.tsx
@@ -14,7 +14,6 @@ import {
   Spacer,
   Input,
   Image,
-  position,
 } from '@chakra-ui/react';
 import { ContextMenu } from 'chakra-ui-contextmenu';
 import CustomAvatar from './CustomAvatar';
@@ -22,158 +21,80 @@ import { OptionMenu } from './OptionMenu';
 import RawUserItemProps from '@/interfaces/RawUserItemProps';
 import RawUserItem from './RawUserItem';
 import { useEffect, useRef, useState } from 'react';
-import { MsgDmProps } from '@/interfaces/MsgProps';
 import { userStore } from '@/stores/userStore';
 import { chatStore } from '@/stores/chatStore';
+import { MsgProps } from '@/interfaces/MsgProps';
 
 function PopoverHoc({ user, msgNum }: RawUserItemProps) {
-  const [msgList, setMsgList] = useState<MsgDmProps[]>([]);
+  const [msgList, setMsgList] = useState<MsgProps[]>([]);
   const [msg, setMsg] = useState<string>('');
   const inputRef = useRef<HTMLInputElement>(null);
-  const [roomNo, setRoomNo] = useState<number>();
+  const [dmRoomNo, setDmRoomNo] = useState<number>();
   const { me } = userStore();
+  const { socket } = chatStore();
 
-  // let socket: any = undefined;
-  // export interface MsgProps {
-  //   username: string;
-  //   text: string;
-  // }
+  function getDmRoomNo() {
+    if (socket === undefined) {
+      console.log('socket is undefined');
+      return;
+    }
+    socket.emit('chatRoomDmMe');
+    socket.once('chatRoomDmMe', ({ chatRoomDm }) => {
+      chatRoomDm.forEach(({ id }: any) => {
+        socket.emit('chatRoomDmById', { chatRoomId: id });
+        socket.once('chatRoomDmById', ({ chatRoomDm }) => {
+          const joinedDmUsers = chatRoomDm.joinedDmUser;
+          if (joinedDmUsers.some(({ user: opp }: any) => opp.id === user.id)) {
+            setDmRoomNo(chatRoomDm.id);
+          }
+        });
+      });
+    });
+  }
 
-  // useEffect(() => {
-  //   setMsgList([
-  //     { username: me.name, text: 'hello' },
-  //     { username: 'tmp', text: 'world!' },
-  //     { username: me.name, text: 'hello' },
-  //     { username: 'tmp', text: 'world!' },
-  //     { username: me.name, text: 'hello' },
-  //     { username: 'tmp', text: 'world!' },
-  //     { username: me.name, text: 'hello' },
-  //     { username: 'tmp', text: 'world!' },
-  //     { username: me.name, text: 'hello' },
-  //     { username: 'tmp', text: 'world!' },
-  //     { username: me.name, text: 'hello' },
-  //     { username: 'tmp', text: 'world!' },
-  //     { username: me.name, text: 'hello' },
-  //     { username: 'tmp', text: 'world!' },
-  //     { username: me.name, text: 'hello' },
-  //     { username: 'tmp', text: 'world!' },
-  //     { username: me.name, text: 'hello' },
-  //     { username: 'tmp', text: 'world!' },
-  //     { username: me.name, text: 'hello' },
-  //     { username: 'tmp', text: 'world!' },
-  //     { username: me.name, text: 'hello' },
-  //     { username: 'tmp', text: 'world!' },
-  //     { username: me.name, text: 'hello' },
-  //     { username: 'tmp', text: 'world!' },
-  //   ]);
-  // }, []);
+  function submitDm() {
+    if (socket === undefined) {
+      console.log('socket is undefined');
+      return;
+    }
+    if (dmRoomNo === undefined) {
+      socket.emit('createChatRoomDm', { targetId: user.id });
+      socket.once('createChatRoomDm', ({ chatRoomDm, message }) => {
+        console.log(message);
+        const newDmRoomNo = chatRoomDm.id;
+        setDmRoomNo(newDmRoomNo);
+
+        // TODO: newDmRoomNo를 이용하여 보내기
+        console.log(`${newDmRoomNo}번 DM 방으로 ${msg}를 보내야 합니다`);
+      });
+      return;
+    }
+    // dmRoomNo를 이용하여 보내기
+    console.log(`${dmRoomNo}번 DM 방으로 ${msg}를 보내야 합니다`);
+  }
 
   function handleSendButtonClicked() {
-    // if (socket === undefined) {
-    //   console.log('socket is undefined');
-    //   return;
-    // }
-    console.log(msg);
+    submitDm();
     setMsg('');
-    if (inputRef.current !== null) inputRef.current.focus();
-    // socket.emit('directMessage', { chatRoomId: roomNo, message: msg });
+    if (inputRef.current !== null) {
+      inputRef.current.focus();
+    }
   }
 
   function handleEnterKeyDown(e: React.KeyboardEvent<HTMLElement>) {
-    // if (socket === undefined) {
-    //   console.log('socket is undefined');
-    //   return;
-    // }
+    if (e.key !== 'Enter') {
+      return;
+    }
     if (e.nativeEvent.isComposing) {
       return;
     }
-    if (e.key === 'Enter') {
-      console.log(msg);
-      setMsg('');
-      // socket.emit('directMessage', { chatRoomId: roomNo, message: msg });
-    }
+    submitDm();
+    setMsg('');
   }
-
-  // async function makeS() {
-  //   makeSocket();
-  // }
-
-  // function checkRoomExist() {
-  //   socket?.once('chatRoomAll', (res) => {
-  //     const allChatRoom = res.chatRoom;
-  //     let roomBool = false;
-  //     allChatRoom.forEach((chatRoom: any)=>{
-  //       if (chatRoom.)
-  //     })
-  //   });
-  // }
-
-  // const { socket, makeSocket } = chatStore();
-  // async function dmStart() {
-  //   if (socket === undefined) {
-  //     await makeS();
-  //   }
-  //   // checkRoomExist();
-  //   // if (socket === undefined) {
-  //   //   console.log('socket is UD dmStart', socket);
-  //   //   return;
-  //   // }
-  //   socket?.emit('createChatRoomDm', {
-  //     targetId: user.id,
-  //   });
-  //   socket?.once('createChatRoomDm', (res) => {
-  //     console.log(res);
-  //     setRoomNo(res.chatRoomDm.id);
-  //   });
-  //   socket?.on('chatRoomDmById', async (res) => {
-  //     console.log(res);
-  //   });
-  //   socket?.on('joinDm', (res) => {
-  //     console.log(res);
-  //     socket.emit('chatRoomDmById', { chatRoomId: roomNo });
-  //   });
-  //   socket?.on('leaveDm', async (res) => {
-  //     console.log(res.message);
-  //     socket.emit('chatRoomDmById', { chatRoomId: roomNo });
-  //   });
-  //   socket?.on('directMessage', (res) => {
-  //     setMsgList((prev) => {
-  //       return [
-  //         ...prev,
-  //         {
-  //           username: res.user.username,
-  //           targetname: res.targetUser.user.username,
-  //           text: res.message,
-  //         },
-  //       ];
-  //     });
-  //   });
-  //   console.log('popover open -> DM START');
-  // }
-  // async function dmOver() {
-  //   if (socket === undefined) {
-  //     console.log('socket is UD dmOver');
-  //     return;
-  //   }
-  //   socket.emit('leaveDm', { targetUserId: me.id, chatRoomId: roomNo });
-  //   socket.off('chatRoomDmById');
-  //   socket.off('joinDm');
-  //   socket.off('leaveDm');
-  //   console.log('popover close -> DM DONE');
-  // }
 
   return (
     //FIXME: placement가 하단에 고정될 수는 없을까?
-    <Popover
-      placement="left"
-      onOpen={() => {
-        // console.log('DMTARGET : ', user.id, user.name);
-        // dmStart();
-      }}
-      onClose={() => {
-        // dmOver();
-      }}
-    >
+    <Popover placement="left" onOpen={getDmRoomNo}>
       <PopoverTrigger>
         <Box>
           <RawUserItem user={user} msgNum={msgNum} />
@@ -198,14 +119,14 @@ function PopoverHoc({ user, msgNum }: RawUserItemProps) {
             <VStack p={5} w="full" height={'40vh'} mt={10} bg="white" overflow="scroll">
               <>
                 {msgList.map((msg, idx) =>
-                  msg.username === me.name && msg.targetname === user.name ? (
+                  msg.username === me.name ? (
                     <Flex key={idx} width="100%">
                       <Spacer />
                       <Flex p={3} borderRadius="20px" bg={'main'} color={'white'} fontSize="2xl">
                         {msg.text}
                       </Flex>
                     </Flex>
-                  ) : msg.username === user.name && msg.targetname === me.name ? (
+                  ) : msg.username === user.name ? (
                     <Flex key={idx} width="100%">
                       <Flex p={3} borderRadius="20px" bg="gray.200" color="black" fontSize="2xl">
                         {msg.text}
@@ -219,12 +140,12 @@ function PopoverHoc({ user, msgNum }: RawUserItemProps) {
             <Spacer />
             <HStack w="full" p={5}>
               <Input
-                pl="40px"
+                pl="20px"
                 mr="20px"
                 h="60px"
                 borderRadius="20px"
                 bg="gray.100"
-                fontSize="2xl"
+                fontSize="xl"
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
                   setMsg(e.target.value);
                 }}
