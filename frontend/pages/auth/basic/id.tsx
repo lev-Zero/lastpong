e08bc835ed16dec37921966dc821756a19452d09
cmoon <chihun0507@naver.com>
2023-01-11 18:00:57 +0900

    feat : 모든 입력창에 enter로 진행 가능하게 수정

--- frontend/pages/auth/basic/id.tsx
+++ frontend/pages/auth/basic/id.tsx
@@ -30,6 +30,11 @@ export default function BasicIdPage() {
       }
     }
   }
+  function handleEnterKeyDown(e: React.KeyboardEvent<HTMLElement>) {
+    if (e.key === 'Enter') {
+      submitUsername();
+    }
+  }
 
   return (
     <VStack spacing={14}>
@@ -44,6 +49,7 @@ export default function BasicIdPage() {
           maxLength={10}
           textTransform="uppercase"
           onChange={handleChange}
+          onKeyDown={handleEnterKeyDown}
         />
       </Box>
       <CustomButton size="lg" onClick={submitUsername}>
