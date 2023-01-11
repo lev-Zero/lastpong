19589856fc3c10820d562fbe94a42f11b14f7b62
Yongjun Park <yopark@student.42seoul.kr>
2023-01-11 17:59:37 +0900

    Docs: init.sh 클러스터에서 프로젝트 돌리는 방법 설명 추가

--- init.sh
+++ init.sh
@@ -5,6 +5,9 @@ brew install nodejs
 
 npm i -g @nestjs/cli
 
+# 아래 세줄 : goinfre에 docker 까는 법
+# 이후 켜진 docker에 설정 아이콘 -> Resources 탭 -> Disk image location을 /goinfre/$USERNAME으로 변경
+# 재부팅 이후 goinfre 안에서 lastpong make
 git clone https://github.com/alexandregv/42toolbox.git $HOME/goinfre/toolbox
 cd $HOME/goinfre/toolbox
 sh ./init_docker.sh
