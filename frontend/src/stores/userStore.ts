3efeab2eeca5f92fb6fe820acd41cdd517baa942
Yongjun Park <yopark@student.42seoul.kr>
2023-01-12 23:48:04 +0900

    Move: allUserStore -> userStore

--- frontend/src/stores/userStore.ts
+++ frontend/src/stores/userStore.ts
@@ -8,24 +8,29 @@ import create from 'zustand';
 interface userStoreProps {
   me: UserProps;
   setMe: (user: UserProps) => void;
-  fetchMe: () => Promise<void>;
+  fetchMe: () => Promise<void> | never; // never -> can throws Error
 
   useOtp: boolean;
-  fetchUseOtp: () => Promise<void>;
-  setUseOtp: (useOtp: boolean) => void;
-  toggleUseOtp: () => Promise<void>;
+  fetchUseOtp: () => Promise<void> | never;
+  setUseOtp: (useOtp: boolean) => void | never;
+  toggleUseOtp: () => Promise<void> | never;
 
   friends: UserProps[];
   setFriends: (friends: UserProps[]) => void;
-  fetchFriends: () => Promise<void>;
-  fetchFriendsStatus: () => Promise<void>;
-  addFriend: (name: string) => Promise<void>;
-  deleteFriend: (name: string) => Promise<void>;
+  fetchFriends: () => Promise<void> | never;
+  fetchFriendsStatus: () => Promise<void> | never;
+  addFriend: (name: string) => Promise<void> | never;
+  deleteFriend: (name: string) => Promise<void> | never;
+
   blockedUsers: UserProps[];
-  setBlockedUsers: (friends: UserProps[]) => void;
-  fetchBlockedUsers: () => Promise<void>;
-  addBlock: (name: string) => Promise<void>;
-  deleteBlock: (name: string) => Promise<void>;
+  setBlockedUsers: (friends: UserProps[]) => void | never;
+  fetchBlockedUsers: () => Promise<void> | never;
+  addBlock: (name: string) => Promise<void> | never;
+  deleteBlock: (name: string) => Promise<void> | never;
+
+  allUsers: UserProps[];
+  setAllUsers: (allUsers: UserProps[]) => void;
+  fetchAllUsers: () => Promise<void> | never;
 }
 
 export const userStore = create<userStoreProps>((set, get) => ({
@@ -39,28 +44,17 @@ export const userStore = create<userStoreProps>((set, get) => ({
   setMe: (user: UserProps) => {
     set((state) => ({ ...state, me: user }));
   },
+  /* @throws Error */
   fetchMe: async () => {
-    try {
-      const rawMe: RawUserProps = await customFetch('GET', '/user/me');
-      const me: UserProps = await convertRawUserToUser(rawMe);
-      get().setMe(me);
-    } catch (e) {
-      if (e instanceof Error) {
-        throw Error(e.message);
-      }
-    }
+    const rawMe: RawUserProps = await customFetch('GET', '/user/me');
+    const me: UserProps = await convertRawUserToUser(rawMe);
+    get().setMe(me);
   },
   useOtp: false,
   fetchUseOtp: async () => {
-    try {
-      const json = await customFetch('GET', '/auth/otp');
-      const useOtp = json.otpOn;
-      get().setUseOtp(useOtp);
-    } catch (e) {
-      if (e instanceof Error) {
-        throw Error(e.message);
-      }
-    }
+    const json = await customFetch('GET', '/auth/otp');
+    const useOtp = json.otpOn;
+    get().setUseOtp(useOtp);
   },
   setUseOtp: (useOtp: boolean) => {
     set((state) => ({ ...state, useOtp }));
@@ -86,18 +80,11 @@ export const userStore = create<userStoreProps>((set, get) => ({
     set((state) => ({ ...state, friends }));
   },
   fetchFriends: async () => {
-    try {
-      const rawFriends = await customFetch('GET', '/user/friend');
-      const friends: UserProps[] = await Promise.all(
-        rawFriends.map(async ({ friend: rawFriend }: any) => convertRawUserToUser(rawFriend))
-      );
-      get().setFriends(friends);
-    } catch (e) {
-      if (e instanceof Error) {
-        console.log(e.message);
-        return;
-      }
-    }
+    const rawFriends = await customFetch('GET', '/user/friend');
+    const friends: UserProps[] = await Promise.all(
+      rawFriends.map(async ({ friend: rawFriend }: any) => convertRawUserToUser(rawFriend))
+    );
+    get().setFriends(friends);
   },
   fetchFriendsStatus: async () => {
     const rawFriends = await customFetch('GET', '/user/friend');
@@ -172,5 +159,16 @@ export const userStore = create<userStoreProps>((set, get) => ({
         throw Error(e.message);
       }
     }
+  },
+  allUsers: [],
+  setAllUsers: (allUsers: UserProps[]) => {
+    set((state) => ({ ...state, allUsers }));
+  },
+  fetchAllUsers: async () => {
+    const rawAllUsers = await customFetch('GET', '/user');
+    const allUsers: UserProps[] = await Promise.all(
+      rawAllUsers.map(async (rawUser: RawUserProps) => await convertRawUserToUser(rawUser))
+    );
+    get().setAllUsers(allUsers);
   },
 }));
