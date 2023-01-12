14b06df9a290933411add53c01ba3cac254f642f
Yongjun Park <yopark@student.42seoul.kr>
2023-01-13 02:44:56 +0900

    Refactor: /auth/basic/id

--- frontend/pages/auth/basic/id.tsx
+++ frontend/pages/auth/basic/id.tsx
@@ -3,60 +3,65 @@ import BasicLayout from '@/layouts/BasicLayout';
 import { userStore } from '@/stores/userStore';
 import { customFetch } from '@/utils/customFetch';
 import { Box, Input, Text, VStack } from '@chakra-ui/react';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { ReactElement, useState } from 'react';
 
 export default function BasicIdPage() {
-  const [username, setUsername] = useState<string>('');
+  const [inputName, setInputName] = useState<string>('');
   const router = useRouter();
   const { fetchMe } = userStore();
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setUsername(e.currentTarget.value.toLowerCase());
+    setInputName(e.currentTarget.value.toLowerCase());
   }
-  async function submitUsername() {
-    if (username.search(/[^A-Za-z0-9ㄱ-ㅎ가-힣]/) > -1) {
-      alert('한글/영어/숫자만 이용할 수 있습니다');
-      return;
-    }
-    try {
-      const json = await customFetch('PATCH', '/user/me', { newUserName: username });
-      fetchMe();
-      console.log(json);
-      router.push('/auth/basic/avatar');
-    } catch (e) {
-      if (e instanceof Error) {
-        alert(e.message);
-      }
-    }
-  }
+
   function handleEnterKeyDown(e: React.KeyboardEvent<HTMLElement>) {
     if (e.key === 'Enter') {
-      submitUsername();
+      submitInputName();
     }
   }
 
+  async function submitInputName() {
+    if (inputName.search(/[^A-Za-z0-9ㄱ-ㅎ가-힣]/) > -1) {
+      alert('한글/영어/숫자만 이용할 수 있습니다');
+      return;
+    }
+    customFetch('PATCH', '/user/me', { newUserName: inputName })
+      .then(() => fetchMe())
+      .then(() => router.push('/auth/basic/avatar'))
+      .catch(console.log);
+  }
+
   return (
-    <VStack spacing={14}>
-      <Box>
-        <Text fontSize="5xl">PLEASE ENTER THE ID</Text>
-        <Input
-          bg="white"
-          color="black"
-          size="lg"
-          textAlign="center"
-          fontSize="2xl"
-          maxLength={10}
-          textTransform="uppercase"
-          onChange={handleChange}
-          onKeyDown={handleEnterKeyDown}
-          autoFocus
-        />
-      </Box>
-      <CustomButton size="lg" onClick={submitUsername}>
-        NEXT
-      </CustomButton>
-    </VStack>
+    <>
+      <Head>
+        <title>기본 정보 입력 | LastPong</title>
+        <meta name="description" content="ft_transcendence project in 42 Seoul" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <VStack spacing={14}>
+        <Box>
+          <Text fontSize="5xl">PLEASE ENTER THE ID</Text>
+          <Input
+            bg="white"
+            color="black"
+            size="lg"
+            textAlign="center"
+            fontSize="2xl"
+            maxLength={10}
+            textTransform="uppercase"
+            onChange={handleChange}
+            onKeyDown={handleEnterKeyDown}
+            autoFocus
+          />
+        </Box>
+        <CustomButton size="lg" onClick={submitInputName}>
+          NEXT
+        </CustomButton>
+      </VStack>
+    </>
   );
 }
 
