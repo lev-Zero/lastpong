944184c9644f77f907f93a0059681afdf7d9e996
choi-sunbin <csn1434@naver.com>
2023-01-13 21:09:30 +0900

    html Event

--- frontend/src/utils/getJwtToken.ts
+++ frontend/src/utils/getJwtToken.ts
@@ -1,9 +1,11 @@
 export function getJwtToken() {
-  const cookies = Object.fromEntries(
-    document.cookie.split(';').map((cookie) => cookie.trim().split('='))
-  );
-  if ('accessToken' in cookies) {
-    return cookies['accessToken'];
+  if (process.browser) {
+    const cookies = Object.fromEntries(
+      document.cookie.split(';').map((cookie) => cookie.trim().split('='))
+    );
+    if ('accessToken' in cookies) {
+      return cookies['accessToken'];
+    }
   }
   return '';
 }
